75013740575e76e6063cb4cecb66bf3ffcef3211
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-15 17:21:35 -0300

    Adding Navbar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <h1 className="display-1">Dog</h1>
       <h1>Dog</h1>
     </div>
   );
