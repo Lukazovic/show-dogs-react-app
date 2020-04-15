3970a9f6c0eb7680b6a93c13ebd2f48ffa6cfe82
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-15 17:52:46 -0300

    Adding DogList Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import DogList from "./DogList";
+import whiskey from "./images/whiskey.jpg";
+import hazel from "./images/hazel.jpg";
+import tubby from "./images/tubby.jpg";
 import "./App.css";
-import Navbar from "./Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <h1 className="display-1">Dog</h1>
-      <h1>Dog</h1>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!",
+        ],
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs.",
+        ],
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food.",
+        ],
+      },
+    ],
+  };
+  render() {
+    return (
+      <Route
+        path="/dogs"
+        render={routesProps => (
+          <DogList {...routesProps} dogs={this.props.dogs} />
+        )}
+      />
+    );
+  }
 }
 
 export default App;
